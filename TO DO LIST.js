9063dbc4a6360a1e1a0ff736c9195162e643f364
brandoshizzle <cathcart.brandon@gmail.com>
2020-11-24 15:42:46 -0700

    update todo list

--- TO DO LIST.js
+++ TO DO LIST.js
@@ -1,4 +1,12 @@
 /*
+Bugs :(
+- Title page rendering extra lines
+- Improve performance - takes too long on big projects
+- If taking more than 30 seconds, suggest selection type format
+*/
+
+/*
+Features
 - Add uppercase first time character is introduced
 - Add option to (CONT'D) automatically if character is repeated
 - Selection: find previous scene header and apply next number (currently just thinks it's the first one)
@@ -7,6 +15,6 @@
 - Check if subscription is still active each month
 */
 
-/* MAYBE
+/* UI Improvements
 - Change UI immediately when license is activated
 */
